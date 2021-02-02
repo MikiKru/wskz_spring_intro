467f5bcb79c6bd5daeeba6a0e8d569d00ed5a652
MikiKru <michal_kruczkowski@o2.pl>
2021-02-02 11:17:32 +0100

    lect17 - Konfigurcja projektu Spring

--- src/main/java/pl/wskz/spring_intro/SpringIntroApplication.java
+++ src/main/java/pl/wskz/spring_intro/SpringIntroApplication.java
@@ -2,7 +2,9 @@ package pl.wskz.spring_intro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SpringIntroApplication {
 
